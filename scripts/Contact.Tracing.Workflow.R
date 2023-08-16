d755bbf4e8fa24f10db4f1fbbb22e554f87df12b
Josh Herbeck <60762264+JHerbeck-IDM@users.noreply.github.com>
2023-08-16 15:08:04 -0700

    Create Contact.Tracing.Workflow.R

---
+++ scripts/Contact.Tracing.Workflow.R
@@ -0,0 +1,9 @@
+### Contact tracking workflow
+
+# First goal:  compare populations of partners named through two ways:
+#             A. HIV-infected index individuals
+#             B. HIV-uninfected index individuals
+
+# Randomly select N group A and N group B individuals
+#   all these individuals should be selected from a bounded time window (1 year?)
+# 
