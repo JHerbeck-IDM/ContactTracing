79f5f9124555fc72509c17afd64d4ed26b67c161
Gregory Hart <ghart@idmod.org>
2023-09-07 09:55:57 -0700

    Finished population subsetting code.

--- HIVContactTracing.r
+++ HIVContactTracing.r
@@ -1,10 +1,30 @@
 library('tidyverse')
 
-getGroups <- function(trace_start_time, trace_end_time, look_back_length){
+getGroups <- function(trace_start_time, trace_end_time, acute_to_trace=TRUE){
+  ### This function is focused on getting the population that is alive during
+  ### the tracing period as well as information about them such as date infected,
+  ### date diagnosed, etc. If these things did not happen for a specific
+  ### individual the dates will be NA. Lastly, this fuction flags four groups
+  ### within in the population: HIV-, HIV+ but undiagnosed, HIV+ prevalence,
+  ### HIV+ incidence.
+  ###
+  ### Inputs:
+  ###    trace_start_time (float): The date when the period for contract tracing
+  ###                              started.
+  ###    trace_end_time (float)  : The date when the period for contract tracing
+  ###                              ended.
+  ###    acute_to_trace (bool)   : This flags whether the incidence infections
+  ###                              (those whose contacts will be traced) are
+  ###                              just those diagnosed during the tracing period
+  ###                              or if they also need to be acute at the time
+  ###                              of diagnosis.
+  ### Return:
+  ###    data frame              : Data frame with the aggregated data.
+  
   # Read in file
   eventReport <- read_csv('ReportEventRecorder.csv')
   
-  # Set a dataframe with a row per individual
+  # Set a data frame with a row per individual
   individuals <- unique(eventReport['Individual_ID'])
   
   # Get the birth time for each individual
@@ -37,21 +57,50 @@ getGroups <- function(trace_start_time, trace_end_time, look_back_length){
                        'OnPreART4')
   diagnosis <- eventReport %>% filter(Event_Name %in% careCasadeEvents) %>%
     group_by(Individual_ID) %>%
-    summarize(Diagnosis = min(Year[Year > Year[Event_Name=='NewInfectionEvent']]),
+    summarize(Infected = min(Year[Event_Name=='NewInfectionEvent']),
+              Diagnosis = min(Year[Year > Year[Event_Name=='NewInfectionEvent']]),
               Stage = WHO_Stage[min(which(Year == min(Year[Year > Year[Event_Name=='NewInfectionEvent']])))]) %>%
-    mutate(Diagnosis = ifelse(is.infinite(Diagnosis), NA, Diagnosis))
+    mutate(Diagnosis = ifelse(is.infinite(Diagnosis), NA, Diagnosis),
+           Infected = ifelse(is.infinite(Infected), NA, Infected))
   individuals <- left_join(individuals, diagnosis, by='Individual_ID')
   
-  # Reduce to only individuals that are alive during the tracing period
-  individuals <- individuals %>% filter(Death < trace_start_time | 
-                                          Birth > trace_end_time)
+  # Get time no longer acute
+  endAcute <- eventReport %>% filter(Event_Name %in% c('HIVInfectionStageEnteredLatent',
+                                                       'HIVInfectionStageEnteredAIDS',
+                                                       'HIVInfectionStageEnteredOnART')) %>%
+    group_by(Individual_ID) %>%
+    summarize(EndAcute = min(Year)) %>%
+    mutate(EndAcute = ifelse(is.infinite(EndAcute), NA, EndAcute))
+  individuals <- left_join(individuals, endAcute, by='Individual_ID')
+  
+  # Reduce to only individuals that are alive for any amount of time during the
+  # tracing period
+  individuals <- individuals %>% filter(Death > trace_start_time & 
+                                          Birth < trace_end_time)
   
   # Create the three subgroups
   individuals['subset'] = 'HIV-'
-  individuals$subset[individuals$Diagnosis>=trace_start_time] = 'Incident_HIV'
-  individuals$subset[individuals$Diagnosis<trace_start_time] = 'Prevalent_HIV'
+  # Set everyone infected by the end of the trace period to the prevalent group
+  individuals$subset[individuals$Infected<trace_end_time] = 'Prevalent_HIV'
+  # In the wiki post incident and prevalent are only diagnosed individuals
+  # Here moved the undiagnosed to a separate category commenting this out will
+  # leave them as prevalent
+  individuals$subset[is.na(individuals$Diagnosis) &
+                       individuals$Infected<trace_end_time] = 'Undiagnosed_HIV'
+  if(acute_to_trace){
+    # Only trace (incident) those that are recently diagnosed and acute
+    individuals$subset[individuals$Diagnosis>=trace_start_time &
+                       individuals$Infected<trace_end_time &
+                       individuals$EndAcute > trace_start_time &
+                       individuals$Diagnosis <= individuals$EndAcute] = 'Incident_HIV'
+  } else{
+    # Trace (incident) all recent diagnosed
+    individuals$subset[individuals$Diagnosis>=trace_start_time &
+                       individuals$Infected<trace_end_time] = 'Incident_HIV'
+  }
   individuals$subset <- factor(individuals$subset, levels=c('Incident_HIV',
                                                             'Prevalent_HIV',
+                                                            'Undiagnosed_HIV',
                                                             'HIV-'))
   
   return(individuals)
